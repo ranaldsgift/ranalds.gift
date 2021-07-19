6151f3b0cdea9fcab0fd92c112694c105c0853df
ranaldsgift <kevinjohncampbell@gmail.com>
2021-07-19 18:05:13 -0400

    Disable background video loading when hidden

--- src/components/app/Background.js
+++ src/components/app/Background.js
@@ -4,10 +4,20 @@ import BackgroundVideo from '../../assets/videos/backgrounds/home.mp4';
 import BackgroundVideoPoster from '../../assets/videos/backgrounds/home-frame.jpg';
 
 function Background(props) {
+    if (props.hideVideo) {    
+        return (
+            <div className="fullscreen-background">
+                <div className="fullscreen-background-image"></div>
+                <div className="background-toggle" onClick={props.toggleBackground} title="Toggle Background Video"></div>
+                <a href="/about" className="rg-icon main-logo">&nbsp;</a>
+            </div>
+        );
+    }
+
     return (
         <div className="fullscreen-background">
             <div className="fullscreen-background-image"></div>
-            <video muted playsinline autoPlay={true} loop={true} poster={BackgroundVideoPoster} className="fullscreen-background-video">                
+            <video muted playsInline autoPlay={true} loop={true} poster={BackgroundVideoPoster} className="fullscreen-background-video">                
                 <source src={BackgroundVideo} type="video/mp4"></source>
             </video>
             <div className="background-toggle" onClick={props.toggleBackground} title="Toggle Background Video"></div>
